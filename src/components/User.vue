fac134e0ae21c09173d0942c7586878228eba961
Loy Hernandez <loyhernandez59@gmail.com>
2019-03-27 19:32:08 -0500

    first commit

---
+++ src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Chang my name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+
+                <!-- prop being pass throug -->
+                <app-user-detail 
+                    :nameProp="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge='age'></app-user-detail> 
+
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                :userAge='editAge'
+                @ageWasEdited="$event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name: 'Loy',
+                age: 40
+            };
+        },
+        methods: {
+            changeName(){
+                this.name = 'Marcos';
+            },
+            resetName(){
+                this.name = 'Loys'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ nameProp }}</p>
+        <p>User age: {{ userAge }}</p>
+        <p>{{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            nameProp: {
+                type: String
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.nameProp.split("").reverse().join("")
+            },
+            // cutom event
+            resetName() {
+                this.nameProp = "Loys";
+                this.$emit('nameWasReset', this.myName); 
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
